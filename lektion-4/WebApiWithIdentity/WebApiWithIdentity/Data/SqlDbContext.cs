8b8d09dd0b4c7fc9ce470ea217462fbf31706545
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2021-03-03 15:27:59 +0100

    Lektion 4

---
+++ lektion-4/WebApiWithIdentity/WebApiWithIdentity/Data/SqlDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiWithIdentity.Models;
+
+namespace WebApiWithIdentity.Data
+{
+    public class SqlDbContext : DbContext
+    {
+        public SqlDbContext(DbContextOptions<SqlDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; } 
+
+    }
+}
