064a0f75897790adf93d9c8612110eb6a28f8ec2
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2021-03-08 10:08:32 +0100

    Lektion 5

--- lektion-4/repetition/WebApiWithKey/BlazorApp/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ lektion-4/repetition/WebApiWithKey/BlazorApp/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -13,77 +13,77 @@ namespace BlazorApp.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "D:\WIN20\win20-aspnetwebapi\lektion-4\repetition\WebApiWithKey\BlazorApp\_Imports.razor"
+#line 1 "d:\WIN20\win20-aspnetwebapi\lektion-4\repetition\WebApiWithKey\BlazorApp\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\WIN20\win20-aspnetwebapi\lektion-4\repetition\WebApiWithKey\BlazorApp\_Imports.razor"
+#line 2 "d:\WIN20\win20-aspnetwebapi\lektion-4\repetition\WebApiWithKey\BlazorApp\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\WIN20\win20-aspnetwebapi\lektion-4\repetition\WebApiWithKey\BlazorApp\_Imports.razor"
+#line 3 "d:\WIN20\win20-aspnetwebapi\lektion-4\repetition\WebApiWithKey\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\WIN20\win20-aspnetwebapi\lektion-4\repetition\WebApiWithKey\BlazorApp\_Imports.razor"
+#line 4 "d:\WIN20\win20-aspnetwebapi\lektion-4\repetition\WebApiWithKey\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\WIN20\win20-aspnetwebapi\lektion-4\repetition\WebApiWithKey\BlazorApp\_Imports.razor"
+#line 5 "d:\WIN20\win20-aspnetwebapi\lektion-4\repetition\WebApiWithKey\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\WIN20\win20-aspnetwebapi\lektion-4\repetition\WebApiWithKey\BlazorApp\_Imports.razor"
+#line 6 "d:\WIN20\win20-aspnetwebapi\lektion-4\repetition\WebApiWithKey\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "D:\WIN20\win20-aspnetwebapi\lektion-4\repetition\WebApiWithKey\BlazorApp\_Imports.razor"
+#line 7 "d:\WIN20\win20-aspnetwebapi\lektion-4\repetition\WebApiWithKey\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "D:\WIN20\win20-aspnetwebapi\lektion-4\repetition\WebApiWithKey\BlazorApp\_Imports.razor"
+#line 8 "d:\WIN20\win20-aspnetwebapi\lektion-4\repetition\WebApiWithKey\BlazorApp\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "D:\WIN20\win20-aspnetwebapi\lektion-4\repetition\WebApiWithKey\BlazorApp\_Imports.razor"
+#line 9 "d:\WIN20\win20-aspnetwebapi\lektion-4\repetition\WebApiWithKey\BlazorApp\_Imports.razor"
 using BlazorApp;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "D:\WIN20\win20-aspnetwebapi\lektion-4\repetition\WebApiWithKey\BlazorApp\_Imports.razor"
+#line 10 "d:\WIN20\win20-aspnetwebapi\lektion-4\repetition\WebApiWithKey\BlazorApp\_Imports.razor"
 using BlazorApp.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "D:\WIN20\win20-aspnetwebapi\lektion-4\repetition\WebApiWithKey\BlazorApp\_Imports.razor"
+#line 11 "d:\WIN20\win20-aspnetwebapi\lektion-4\repetition\WebApiWithKey\BlazorApp\_Imports.razor"
 using SharedLibrary.Models;
 
 #line default
---
+++ lektion-5/repetition/WebApiWithAuth/Data/SqlDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiWithAuth.Data
+{
+    public class SqlDbContext : DbContext
+    {
+        public SqlDbContext(DbContextOptions<SqlDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
